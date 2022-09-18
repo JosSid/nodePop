853130c1d22e548698c00ecec5cf68379d3e587a
Josep Valls <alfredvays@gmail.com>
2022-09-18 08:05:17 +0200

    Añado ESLint

--- data/instrumentos.js
+++ data/instrumentos.js
@@ -1,63 +1,60 @@
-'use strict'
-
 const instrumentos = [
-    {
-        name: 'Guitarra Aria pro2',
-        sale: true,
-        price: 350.00,
-        photo: 'ariaPro2.png',
-        tags: ['guitar', 'instrument', 'chord', 'electric']
-    },
-    {
-        name: 'Guitarra Electrica',
-        sale: false,
-        price: 1000.00,
-        photo: 'ariaCMac.png',
-        tags: ['guitar', 'instrument', 'chord', 'electric']
-    },
-    {
-        name: 'Guitarra Española Josep Penades',
-        sale: true,
-        price: 550.00,
-        photo: 'acustic.png',
-        tags: ['guitar', 'instrument', 'chord', 'acustic']
-    },
-    {
-        name: 'Flauta travesera',
-        sale: true,
-        price: 850.00,
-        photo: 'flauta.png',
-        tags: ['acustic', 'instrument', 'wind', 'wood']
-    },
-    {
-        name: 'Amplificador Marshall Combo 100',
-        sale: false,
-        price: 1500.00,
-        photo: 'marshall.png',
-        tags: ['accesory', 'booster', 'electric','guitar']
-    },
-    {
-        name: 'Pedal Metal Zone',
-        sale: false,
-        price: 150.00,
-        photo: 'metalZone.png',
-        tags: ['accesory', 'guitar', 'effects', 'electric']
-    },
-    {
-        name: 'Atril',
-        sale: true,
-        price: 90.00,
-        photo: 'atril.png',
-        tags: ['accesory']
-    },
-    {
-        name: 'Soporte Guitarra',
-        sale: true,
-        price: 40.00,
-        photo: 'soporteGuitarra.png',
-        tags: ['accesory', 'guitar']
-    }
+  {
+    name: "Guitarra Aria pro2",
+    sale: true,
+    price: 350.0,
+    photo: 'ariaPro2.png',
+    tags: ['guitar', 'instrument', 'chord', 'electric'],
+  },
+  {
+    name: 'Guitarra Electrica',
+    sale: false,
+    price: 1000.0,
+    photo: 'ariaCMac.png',
+    tags: ['guitar', 'instrument', 'chord', 'electric'],
+  },
+  {
+    name: 'Guitarra Española Josep Penades',
+    sale: true,
+    price: 550.0,
+    photo: 'acustic.png',
+    tags: ['guitar', 'instrument', 'chord', 'acustic'],
+  },
+  {
+    name: 'Flauta travesera',
+    sale: true,
+    price: 850.0,
+    photo: 'flauta.png',
+    tags: ['acustic', 'instrument', 'wind', 'wood'],
+  },
+  {
+    name: 'Amplificador Marshall Combo 100',
+    sale: false,
+    price: 1500.0,
+    photo: 'marshall.png',
+    tags: ['accesory', 'booster', 'electric', 'guitar'],
+  },
+  {
+    name: 'Pedal Metal Zone',
+    sale: false,
+    price: 150.0,
+    photo: 'metalZone.png',
+    tags: ['accesory', 'guitar', 'effects', 'electric'],
+  },
+  {
+    name: 'Atril',
+    sale: true,
+    price: 90.0,
+    photo: 'atril.png',
+    tags: ['accesory'],
+  },
+  {
+    name: 'Soporte Guitarra',
+    sale: true,
+    price: 40.0,
+    photo: 'soporteGuitarra.png',
+    tags: ['accesory', 'guitar'],
+  },
 ];
 
 module.exports = instrumentos;
-
